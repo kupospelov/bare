18dab1fb48e26ec526d3a1806e7e54a19a8d5c7e
Konstantin Pospelov <kupospelov@gmail.com>
2026-06-10 19:42:26 +0200

    volume: use pipewire-native

--- src/blocks/volume.rs
+++ src/blocks/volume.rs
@@ -2,19 +2,25 @@ use super::{Block, Fd, Instance};
 use crate::config::{ColorConfig, VolumeConfig, VolumeFormatItem};
 use crate::render;
 use crate::state::State;
-use crate::{debug, error};
-use pipewire as pw;
-use pw::spa::param::ParamType;
-use pw::spa::pod::deserialize::PodDeserializer;
-use pw::spa::pod::{Pod, Value, ValueArray};
-use pw::spa::sys::{
-    SPA_PROP_channelVolumes as SPA_PROP_CHANNEL_VOLUMES, SPA_PROP_mute as SPA_PROP_MUTE,
+use crate::{debug, error, warning};
+use pipewire_native::{
+    self as pipewire,
+    context::Context,
+    main_loop::MainLoop,
+    properties::Properties,
+    proxy::metadata::MetadataEvents,
+    proxy::node::NodeEvents,
+    proxy::{ProxyEvents, metadata::Metadata, node::Node, registry::RegistryEvents},
+    some_closure, types,
 };
-use pw::types::ObjectType;
-use std::cell::RefCell;
-use std::collections::HashMap;
-use std::os::fd::AsRawFd;
-use std::rc::Rc;
+use pipewire_native_spa::{
+    param::{ParamType, props::Prop},
+    pod::{RawPodOwned, parser::Parser},
+};
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock},
+};
 
 #[derive(Default, Clone, PartialEq)]
 struct SinkState {
@@ -60,8 +66,88 @@ impl Group {
             return;
         }
 
-        let pw = PipeWire::new().expect("Failed to connect to PipeWire");
-        let fd = pw.main_loop.loop_().fd().as_raw_fd();
+        pipewire::init();
+        debug!("PipeWire initialized");
+
+        // The library requires captured variables to satisfy Send.
+        let sinks = Arc::new(RwLock::new(Sinks {
+            default_sink: None,
+            sinks: HashMap::new(),
+        }));
+
+        let properties = Properties::new();
+        let main_loop = MainLoop::new(&properties).unwrap();
+        let context = Context::new(&main_loop, properties).expect("Failed to create context");
+        let core = context
+            .connect(None)
+            .expect("Failed to connect to the server");
+        let registry = core.registry().expect("Failed to create registry");
+
+        registry.add_listener(RegistryEvents {
+            global: some_closure!([registry ^(sinks)] id, _perms, interface, version, props, {
+                match interface {
+                    types::interface::METADATA => {
+                        let object = registry.bind(id, interface, version).unwrap();
+
+                        let metadata = object.downcast::<Metadata>().unwrap();
+                        metadata.add_listener({
+                            let sinks = sinks.clone();
+                            MetadataEvents {
+                                property: Some(Box::new(move |_id, key, _type, value| {
+                                    if key == Some("default.audio.sink") {
+                                        let name = value.and_then(|v| v.split('"').nth(3)).map(|name| name.to_string());
+
+                                        debug!("Default sink = {:?}", name);
+                                        sinks.write().unwrap().default_sink = name;
+                                    }
+                                })),
+                            }
+                        });
+
+                        let proxy = object.downcast_proxy::<Metadata>().unwrap();
+                        proxy.add_listener(ProxyEvents {
+                            removed: some_closure!([] {}),
+                            ..Default::default()
+                        });
+                    },
+                    types::interface::NODE => {
+                        let Some(name) = props.get("node.name").map(|s| s.to_owned()) else {
+                            warning!("Node does not have a name");
+                            return;
+                        };
+
+                        let object = registry.bind(id, interface, version).unwrap();
+                        let node = object.downcast::<Node>().unwrap();
+                        node.subscribe_params(&[ParamType::Props])
+                            .expect("Failed to subscribe node");
+                        node.add_listener(NodeEvents {
+                            info: Some(Box::new(move |_| {})),
+
+                            param: Some(Box::new({
+                                let sinks = sinks.clone();
+                                move |_, param_type, _, _, pod: &RawPodOwned| {
+                                    if param_type == ParamType::Props {
+                                        update_sink_volume(&name, pod, &sinks);
+                                    }
+                                }
+                            })),
+                        });
+
+                        let proxy = object.downcast_proxy::<Node>().unwrap();
+                        proxy.add_listener(ProxyEvents {
+                            removed: some_closure!([] {}),
+                            ..Default::default()
+                        });
+                    },
+                    _ => {
+                        return;
+                    }
+                };
+            }),
+            global_remove: some_closure!([] _id, {}),
+        });
+
+        let fd = main_loop.get_fd();
         handle
             .insert_source(
                 calloop::generic::Generic::new(
@@ -70,11 +156,11 @@ impl Group {
                     calloop::Mode::Level,
                 ),
                 move |_, _, state| {
-                    // Bind pw to capture the whole struct.
-                    let _ = &pw;
+                    // Capture context.
+                    let _ = &context;
 
-                    pw.main_loop.loop_().iterate(std::time::Duration::ZERO);
-                    let current = pw.sinks.borrow().current();
+                    let _ = main_loop.iterate(Some(std::time::Duration::ZERO));
+                    let current = sinks.read().unwrap().current();
 
                     for i in 0..state.blocks.volume.instances.len() {
                         let id = {
@@ -95,39 +181,50 @@ impl Group {
     }
 }
 
-struct PipeWire {
-    main_loop: pw::main_loop::MainLoopRc,
-    _context: pw::context::ContextRc,
-    _core: pw::core::CoreRc,
-    _registry: pw::registry::RegistryRc,
-    _registry_listener: pw::registry::Listener,
-    _proxies: Rc<RefCell<HashMap<u32, ProxyEntry>>>,
-    sinks: Rc<RefCell<Sinks>>,
-}
+fn update_sink_volume(node_name: &str, pod: &RawPodOwned, sinks: &Arc<RwLock<Sinks>>) {
+    let mut parser = Parser::new(pod.data());
+    let mut volume = None;
+    let mut mute = None;
+    let result = parser.pop_object_raw(|p, _type, _id: u32| {
+        for (key, _flags, value) in p {
+            let Ok(key) = Prop::try_from(key) else {
+                warning!("Skipping unknown key: {}", key);
+                continue;
+            };
 
-struct ProxyEntry {
-    _proxy: Box<dyn pw::proxy::ProxyT>,
-    _listener: Box<dyn pw::proxy::Listener>,
-}
+            match key {
+                Prop::ChannelVolumes => {
+                    if let Ok(value) = value.decode::<Vec<f32>>() {
+                        let max = value.iter().fold(0.0_f32, |m, v| f32::max(m, *v)).max(0.0);
+                        let percent = (max.cbrt() * 100.0).round();
+                        volume = Some(percent.clamp(0.0, 255.0) as u8);
+                    }
+                }
+                Prop::Mute => {
+                    if let Ok(value) = value.decode::<bool>() {
+                        mute = Some(value);
+                    }
+                }
+                _ => {}
+            }
+        }
+        Ok(())
+    });
 
-impl PipeWire {
-    fn new() -> Result<Self, Box<dyn std::error::Error>> {
-        let main_loop = pw::main_loop::MainLoopRc::new(None)?;
-        let context = pw::context::ContextRc::new(&main_loop, None)?;
-        let core = context.connect_rc(None)?;
-        let registry = core.get_registry_rc()?;
-        let sinks = Rc::new(RefCell::new(Sinks::default()));
-        let proxies: Rc<RefCell<HashMap<u32, ProxyEntry>>> = Rc::new(RefCell::new(HashMap::new()));
-        let registry_listener = build_registry_listener(&registry, sinks.clone(), proxies.clone());
-        Ok(Self {
-            main_loop,
-            _context: context,
-            _core: core,
-            _registry: registry,
-            _registry_listener: registry_listener,
-            _proxies: proxies,
-            sinks,
-        })
+    if let Err(e) = result {
+        error!("Failed to parse volume: {:?}", e);
+        return;
+    }
+
+    if volume.is_some() || mute.is_some() {
+        let mut sinks = sinks.write().unwrap();
+        let sink = sinks.sinks.entry(node_name.to_string()).or_default();
+        if let Some(v) = volume {
+            sink.percent = Some(v);
+        }
+        if let Some(m) = mute {
+            sink.mute = m;
+        }
     }
 }
 
@@ -231,141 +328,4 @@ impl Block for Volume {
             y += h as i32 + margin;
         }
     }
-}
-
-fn build_registry_listener(
-    registry: &pw::registry::RegistryRc,
-    sinks: Rc<RefCell<Sinks>>,
-    proxies: Rc<RefCell<HashMap<u32, ProxyEntry>>>,
-) -> pw::registry::Listener {
-    let registry_weak = registry.downgrade();
-    registry
-        .add_listener_local()
-        .global({
-            let proxies = proxies.clone();
-            move |obj| {
-                let Some(registry) = registry_weak.upgrade() else {
-                    return;
-                };
-                let entry = match obj.type_ {
-                    ObjectType::Metadata => bind_default_metadata(&registry, obj, &sinks),
-                    ObjectType::Node => bind_audio_sink(&registry, obj, &sinks),
-                    _ => return,
-                };
-                if let Some(entry) = entry {
-                    proxies.borrow_mut().insert(obj.id, entry);
-                }
-            }
-        })
-        .global_remove(move |id| {
-            proxies.borrow_mut().remove(&id);
-        })
-        .register()
-}
-
-fn bind_default_metadata(
-    registry: &pw::registry::RegistryRc,
-    obj: &pw::registry::GlobalObject<&pw::spa::utils::dict::DictRef>,
-    sinks: &Rc<RefCell<Sinks>>,
-) -> Option<ProxyEntry> {
-    let props = obj.props?;
-    if props.get("metadata.name") != Some("default") {
-        return None;
-    }
-    let metadata: pw::metadata::Metadata = registry
-        .bind(obj)
-        .inspect_err(|e| error!("Failed to bind default metadata: {:?}", e))
-        .ok()?;
-    let listener = metadata
-        .add_listener_local()
-        .property({
-            let sinks = sinks.clone();
-            move |_subject, key, _type_, value| {
-                if key == Some("default.audio.sink") {
-                    // Parse JSON like `{"name":"sink_name"}`
-                    if let Some(name) = value.and_then(|v| v.split('"').nth(3)) {
-                        debug!("Default sink = {}", name);
-                        sinks.borrow_mut().default_sink = Some(name.to_string());
-                    }
-                }
-                0
-            }
-        })
-        .register();
-    Some(ProxyEntry {
-        _proxy: Box::new(metadata),
-        _listener: Box::new(listener),
-    })
-}
-
-fn bind_audio_sink(
-    registry: &pw::registry::RegistryRc,
-    obj: &pw::registry::GlobalObject<&pw::spa::utils::dict::DictRef>,
-    sinks: &Rc<RefCell<Sinks>>,
-) -> Option<ProxyEntry> {
-    let props = obj.props?;
-    if props.get("media.class") != Some("Audio/Sink") {
-        return None;
-    }
-    let name = props.get("node.name")?.to_string();
-    let node: pw::node::Node = registry
-        .bind(obj)
-        .inspect_err(|e| error!("Failed to bind sink {}: {:?}", obj.id, e))
-        .ok()?;
-    node.subscribe_params(&[ParamType::Props]);
-    let listener = node
-        .add_listener_local()
-        .param({
-            let sinks = sinks.clone();
-            move |_seq, _id, _idx, _next, pod| {
-                let Some(pod) = pod else { return };
-                let Some((vol, mute)) = parse_props(pod) else {
-                    return;
-                };
-                let mut s = sinks.borrow_mut();
-                let entry = s.sinks.entry(name.clone()).or_default();
-                if let Some(v) = vol {
-                    entry.percent = Some(v);
-                    debug!("Volume {}: {}%", name, v);
-                }
-                if let Some(m) = mute {
-                    entry.mute = m;
-                    debug!("Volume {}: mute={}", name, m);
-                }
-            }
-        })
-        .register();
-    Some(ProxyEntry {
-        _proxy: Box::new(node),
-        _listener: Box::new(listener),
-    })
-}
-
-fn parse_props(pod: &Pod) -> Option<(Option<u8>, Option<bool>)> {
-    let (_, value) = PodDeserializer::deserialize_any_from(pod.as_bytes()).ok()?;
-    let Value::Object(obj) = value else {
-        return None;
-    };
-    let mut vol = None;
-    let mut mute = None;
-    for prop in obj.properties {
-        match prop.key {
-            SPA_PROP_CHANNEL_VOLUMES => {
-                if let Value::ValueArray(ValueArray::Float(arr)) = prop.value
-                    && !arr.is_empty()
-                {
-                    let max = arr.iter().fold(0.0_f32, |m, v| f32::max(m, *v)).max(0.0);
-                    let percent = (max.cbrt() * 100.0).round();
-                    vol = Some(percent.clamp(0.0, 255.0) as u8);
-                }
-            }
-            SPA_PROP_MUTE => {
-                if let Value::Bool(b) = prop.value {
-                    mute = Some(b);
-                }
-            }
-            _ => {}
-        }
-    }
-    Some((vol, mute))
 }
